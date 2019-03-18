a52840c4f6c981cae64c131908e622bbca1107a1
alfonso.benavides <abenavid@gmail.com>
2019-03-18 00:01:39 -0300

    module config

--- src/components/bootstrap/ModuleAutoLoader.php
+++ src/components/bootstrap/ModuleAutoLoader.php
@@ -23,13 +23,24 @@ class ModuleAutoLoader implements BootstrapInterface
 {
     /** @var array Model's map */
     private $_modelMap = [
-
+        'Core' => 'primaria\core\models\Core',
     ];
 
     /** @inheritdoc */
     public function bootstrap($app){
         if ($app->hasModule('core') && ($module = $app->getModule('core')) instanceof Module) {
             $this->_modelMap = array_merge($this->_modelMap, $module->modelMap);
+            foreach ($this->_modelMap as $name => $definition) {
+                $class = "primaria\\core\\models\\" . $name;
+                Yii::$container->set($class, $definition);
+                $modelName = is_array($definition) ? $definition['class'] : $definition;
+                $module->modelMap[$name] = $modelName;
+                if (in_array($name, ['Core', 'Profile', 'Token', 'Account'])) {
+                    Yii::$container->set($name . 'Query', function () use ($modelName) {
+                        return $modelName::find();
+                    });
+                }
+            }
 
             $configUrlRule = [
                 'prefix' => $module->urlPrefix,
