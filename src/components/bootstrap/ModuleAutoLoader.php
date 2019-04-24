27f0bb387acd3335b9e6e108af7d34d1f4d5235e
alfonso.benavides <abenavid@gmail.com>
2019-04-24 12:14:45 -0400

    autoload

--- src/components/bootstrap/ModuleAutoLoader.php
+++ src/components/bootstrap/ModuleAutoLoader.php
@@ -58,7 +58,7 @@ class ModuleAutoLoader implements BootstrapInterface
             Yii::$app->cache->set(self::CACHE_ID, $modules);
         }
         print_r($modules);
-
+        die();
         return $modules;
     }
 
